2386df728af864d69d09af60529056b489ffedd1
Rakesh HS <126412041+Rakesh-Seenu@users.noreply.github.com>
2025-05-21 14:44:19 +0200

    workflow updation

--- vpeleaderboard/data/src/SBML/__init__.py
+++ vpeleaderboard/data/src/SBML/__init__.py
@@ -3,11 +3,9 @@
 from . import basico_model
 from . import sys_bio_model
 from . import sbml_dataloader
-from . import models
 
 __all__ = [
     "basico_model",
     "sys_bio_model",
     "sbml_dataloader",
-    "models"
 ]
